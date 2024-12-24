ddddd934a39de346ddbce05b788d58c17f9f8d01
Ayush Sahrawat <ayushsahrawat47@gmail.com>
2024-12-24 22:02:46 +0530

    cors added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const jobRoutes = require('./routes/jobRoutes');
+const cors = require('cors')
 require('dotenv').config();
 
 
@@ -9,6 +10,7 @@ const app = express();
 connectDB();
 const PORT = process.env.PORT || 5000; 
 
+app.use(cors());
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
